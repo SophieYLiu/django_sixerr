1fb4e459b60283ae755b856ba463b2ef6a81d7b7
Andrew-Chen928 <ichiro710928@gmail.com>
2016-11-02 19:29:44 -0700

    fix CSS import problem

--- sixerrapp/static/js/common.js
+++ sixerrapp/static/js/common.js
@@ -1,11 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    //var prises = document.querySelectorAll('[id^=price]')
-    var gig_home_page = $('#gig_home_page');
-    console.log(gig_home_page.length);
     var size = document.querySelectorAll('[class^=green]').length;
     for ( var i = 1; i <= size; i++ ){
-    	console.log("Hey, Mom ------ ");
     	var counter = i;
     	var price_id = "price-"+counter.toString();
     	var price = document.getElementById(price_id).innerHTML;
